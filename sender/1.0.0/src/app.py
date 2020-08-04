a77ed868f3df3ed2bbadf9ccef77abe2a2cf2d3b
social-ran <68770158+social-ran@users.noreply.github.com>
2020-08-04 09:29:27 +0800

    Update app.py

--- sender/1.0.0/src/app.py
+++ sender/1.0.0/src/app.py
@@ -25,7 +25,7 @@ class sender(AppBase):
         fp.write(speed)
         fp.write('",\n"size_Cauchy1":"500",\n"size_Cauchy2":"1"\n}\n}\n')
         os.system('./tra_docker_make/run.sh')
-        return a+"tcpsender finished!"
+        return "tcpsender finished!"
     async def udpsender(self,ip,speed,start_time="2018-08-11 13:41:11",end_time="2018-08-11 13:41:41",log_path="/var/log/appsimulation/traffic_gen.log"):
         fp = open('/home/config/post_info.json', 'w')
         fp.write('{\n"cmd_info":{\n"cmd":"start"\n},\n"task_info":{\n"log_path":"')
